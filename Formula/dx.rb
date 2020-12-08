785be09eedea1b28ec5e6e7ade2d11792e060d74
garethjevans <gareth@bryncynfelin.co.uk>
2020-12-08 10:41:19 +0000

    Brew formula update for dx version 0.0.16

--- Formula/dx.rb
+++ Formula/dx.rb
@@ -2,16 +2,16 @@
 class Dx < Formula
   desc "Have you got the chillys?"
   homepage ""
-  version "0.0.15"
+  version "0.0.16"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/plumming/dx/releases/download/0.0.15/dx-darwin-amd64.tar.gz"
-    sha256 "bc1ef6c8c4441678c4b2070d9185d63982ec8a3d5b87d56928ea0986fd5eccb4"
+    url "https://github.com/plumming/dx/releases/download/0.0.16/dx-darwin-amd64.tar.gz"
+    sha256 "85051c06b650761a573adcf7c60fccf715c160e1a286bd017880b37609601ebb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/plumming/dx/releases/download/0.0.15/dx-linux-amd64.tar.gz"
-    sha256 "94b0d91f2713de91ac6f39a220fbb8f2ee0aaeb703a06a32b13f68c37bc9ab86"
+    url "https://github.com/plumming/dx/releases/download/0.0.16/dx-linux-amd64.tar.gz"
+    sha256 "d8b62c4e0c0e568126cb65e22f76b8302a25c64166a3a309e831b0ccc11c49e2"
   end
 
   def install
