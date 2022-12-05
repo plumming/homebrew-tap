55b9212c988f7bed866d3073dd876ec1253a5f45
garethjevans <gareth@bryncynfelin.co.uk>
2022-12-05 10:11:19 +0000

    Brew formula update for dx version 0.3.0

--- Formula/dx.rb
+++ Formula/dx.rb
@@ -5,20 +5,20 @@
 class Dx < Formula
   desc "Have you got the chillys?"
   homepage ""
-  version "0.2.6"
+  version "0.3.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/plumming/dx/releases/download/0.2.6/dx-darwin-arm64.tar.gz"
-      sha256 "d36669ae8cf960751c43a8e1c5b3896a2960154b96ae17127c5d264637e8237c"
+    if Hardware::CPU.intel?
+      url "https://github.com/plumming/dx/releases/download/0.3.0/dx-darwin-amd64.tar.gz"
+      sha256 "1c4a4952b8f1e9e6ad1983d79cb033ca5b3e6497dfd90e89eedb743524333e9a"
 
       def install
         bin.install "dx"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/plumming/dx/releases/download/0.2.6/dx-darwin-amd64.tar.gz"
-      sha256 "d083504a18c9562a2b3dcef64a3a5d87725898cd7c4ef41fcb9073e3c78922d4"
+    if Hardware::CPU.arm?
+      url "https://github.com/plumming/dx/releases/download/0.3.0/dx-darwin-arm64.tar.gz"
+      sha256 "994c9f4715f792e7437f29b27b6d9b564923aff89e9ca8a2b1e32b98d04a90be"
 
       def install
         bin.install "dx"
@@ -28,16 +28,16 @@ class Dx < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/plumming/dx/releases/download/0.2.6/dx-linux-arm64.tar.gz"
-      sha256 "db936a1b4a3d8f29f108d8f066c023d17a7ea8b0417032fa9b05d3f3f675b5aa"
+      url "https://github.com/plumming/dx/releases/download/0.3.0/dx-linux-arm64.tar.gz"
+      sha256 "1146e9c5195380863b70ffc3438e4af2fb91f7bee606f285f64031b91746458b"
 
       def install
         bin.install "dx"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/plumming/dx/releases/download/0.2.6/dx-linux-amd64.tar.gz"
-      sha256 "60227fb349f228684d430ef2354ff85104ef78cafde2ac4c96615a315f67f564"
+      url "https://github.com/plumming/dx/releases/download/0.3.0/dx-linux-amd64.tar.gz"
+      sha256 "c46fad1fc9adcf806245507ac4baf8d628071322fa1c579b96c517fb082cb7b7"
 
       def install
         bin.install "dx"
